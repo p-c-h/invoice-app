0502412b8b7baea499afc93b4767f8b768e29bdc
p-c-h <105007901+Pi-code246@users.noreply.github.com>
2023-03-20 23:39:21 +0100

    Enable invoice_pdf function to display data in a table

--- controllers/invoiceController.js
+++ controllers/invoiceController.js
@@ -2,7 +2,8 @@ const { body, validationResult } = require("express-validator");
 const async = require("async");
 const Buyer = require("../models/buyer");
 const Invoice = require("../models/invoice");
-const PDFDocument = require("pdfkit");
+// const PDFDocument = require("pdfkit");
+const PDFDocument = require("pdfkit-table");
 const path = require("path");
 
 function adjacent(date) {
@@ -343,8 +344,13 @@ exports.invoice_pdf = function (req, res, next) {
       if (err) {
         return next(err);
       }
-      const { dateCreated, invoiceNumber, transactionDate, paymentMethod } =
-        result;
+      const {
+        dateCreated,
+        invoiceNumber,
+        transactionDate,
+        paymentMethod,
+        paymentDue,
+      } = result;
 
       const month = result.transactionDate.getMonth() + 1;
       const year = result.transactionDate.getFullYear();
@@ -361,22 +367,111 @@ exports.invoice_pdf = function (req, res, next) {
         .text(`Wystawiono dnia ${formatDate(dateCreated)}, Kielce`);
       doc.moveDown();
 
-      doc.x = 300;
+      let startPosition = (doc.x = 300);
       doc
         .fontSize(12)
         .text(`Faktura VAT nr ${invoiceNumber + "/" + month + "/" + year}`);
 
-      const linePosition = doc.y;
+      let linePosition = doc.y;
+
       doc.text("Data sprzedaży:");
 
-      doc.text(formatDate(transactionDate), doc.x + 100, linePosition);
+      doc.text(formatDate(transactionDate), startPosition + 150, linePosition);
+      linePosition = doc.y;
 
-      doc.text("Sposób zapłaty:");
+      doc.text("Sposób zapłaty:", startPosition);
 
       doc.text(
         paymentMethod === "transfer" ? "Przelew" : "Gotówka",
-        doc.x + 100
+        startPosition + 150,
+        linePosition
       );
+
+      linePosition = doc.y;
+      doc.text("Termin płatności:", startPosition);
+      doc
+        .text(`${paymentDue}`, startPosition + 150, linePosition)
+
+        .moveDown(0.5);
+
+      doc.x = 72;
+
+      const table = {
+        title: "Title",
+        subtitle: "Subtitle",
+        headers: [
+          { label: "Name", property: "name", width: 60, renderer: null },
+          {
+            label: "Description",
+            property: "description",
+            width: 150,
+            renderer: null,
+          },
+          { label: "Price 1", property: "price1", width: 100, renderer: null },
+          { label: "Price 2", property: "price2", width: 100, renderer: null },
+          { label: "Price 3", property: "price3", width: 80, renderer: null },
+          {
+            label: "Price 4",
+            property: "price4",
+            width: 43,
+            renderer: (
+              value,
+              indexColumn,
+              indexRow,
+              row,
+              rectRow,
+              rectCell
+            ) => {
+              return `U$ ${Number(value).toFixed(2)}`;
+            },
+          },
+        ],
+        // complex data
+        datas: [
+          {
+            name: "Name 1",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis ante in laoreet egestas. ",
+            price1: "$1",
+            price3: "$ 3",
+            price2: "$2",
+            price4: "4",
+          },
+          {
+            options: { fontSize: 10, separation: true },
+            name: "bold:Name 2",
+            description: "bold:Lorem ipsum dolor.",
+            price1: "bold:$1",
+            price3: {
+              label: "PRICE $3",
+              options: { fontSize: 12 },
+            },
+            price2: "$2",
+            price4: "4",
+          },
+          // {...},
+        ],
+        // simeple data
+        rows: [
+          [
+            "Apple",
+            "Nullam ut facilisis mi. Nunc dignissim ex ac vulputate facilisis.",
+            "$ 105,99",
+            "$ 105,99",
+            "$ 105,99",
+            "105.99",
+          ],
+          // [...],
+        ],
+      };
+      // the magic
+      doc.table(table, {
+        prepareHeader: () => doc.font("Helvetica-Bold").fontSize(8),
+        prepareRow: (row, indexColumn, indexRow, rectRow, rectCell) => {
+          doc.font("Helvetica").fontSize(8);
+          indexColumn === 0 && doc.addBackground(rectRow, "blue", 0.15);
+        },
+      });
 
       doc.pipe(res);
       doc.end();
