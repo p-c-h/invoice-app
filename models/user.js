6a18944041b906231c4dcee6963b8452bb75f357
p-c-h <105007901+Pi-code246@users.noreply.github.com>
2023-04-12 22:33:16 +0200

    Remove required entry from firstName and surname in user model

--- models/user.js
+++ models/user.js
@@ -14,11 +14,9 @@ const UserSchema = new Schema({
   //
   firstName: {
     type: String,
-    required: true,
   },
   surname: {
     type: String,
-    required: true,
   },
   //
   businessName: {
