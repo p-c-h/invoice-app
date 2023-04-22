bb6daff99185a87275625395caf4baeeea648a19
p-c-h <105007901+Pi-code246@users.noreply.github.com>
2023-04-22 20:36:21 +0200

    Add formatAccNum()

--- utils/utils.js
+++ utils/utils.js
@@ -2,6 +2,14 @@ function formatPrice(arg) {
   return Number(arg).toFixed(2).replace(".", ",");
 }
 
+function formatAccNum(accNum) {
+  const firstTwo = accNum.slice(0, 2);
+  const rest = accNum.slice(2);
+  const fours = rest.match(/.{1,4}/g);
+  return [firstTwo, ...fours].join(" ");
+}
+
 module.exports = {
   formatPrice,
+  formatAccNum,
 };
